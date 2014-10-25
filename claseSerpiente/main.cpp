83d454a44d9b500cf85791dbfb2d690051f9ddee
JJLionHeart <jj.lopezjaimez@gmail.com>
2014-10-24 23:16:48 -0500

    Cambio direccionn
    detecta tecla pulsada para cambio direccion

--- claseSerpiente/main.cpp
+++ claseSerpiente/main.cpp
@@ -7,11 +7,38 @@ int main()
     Vibora a;
     tablero t;
     a.dibujarVibora(t);
-    for(int i=0;i<200;i++){
+    short int u,d,r,l;
+    while(true){
+        if((u=GetAsyncKeyState(VK_UP))||(d=GetAsyncKeyState(VK_DOWN))||(r=GetAsyncKeyState(VK_RIGHT))||(l=GetAsyncKeyState(VK_LEFT))){
+            u=u>>16;
+            u=u&0x1;
+            d=d>>16;
+            d=d&0x1;
+            r=r>>16;
+            r=r&0x1;
+            l=l>>16;
+            l=l&0x1;
+            if(u){
+                    cout<<"up"<<endl;
+                a.Modificar(1)
+            }else if(r){
+                cout<<"Right"<<endl;
+                a.Modificar(2);
+            }else if(d){
+                cout<<"down"<<endl;
+                a.Modificar(3);
+            }else if(l){
+                cout<<"left"<<endl;
+                a.Modificar(4);
+            }
+        }else{
+
         a.Mover();
         Sleep(10);
         t.LimpiarTablero();
         a.dibujarVibora(t);
+
+        }
     }
     return 0;
 }
