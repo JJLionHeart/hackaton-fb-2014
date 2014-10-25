ffdac0604615e3f7f838a77fc23ee6b419a8a46a
HumbertoG <jhgm_1995@hotmail.com>
2014-10-24 23:43:38 -0500

    cuadrito con direccion
    se agrego direccion

--- claseSerpiente/cuadrito.h
+++ claseSerpiente/cuadrito.h
@@ -9,13 +9,16 @@ public:
     void setX(int iX);
     void setY(int iY);
     void setActivo(bool iActivo);
+    void setDireccion(int dire);
     int getX();
     int getY();
     bool getActivo();
+    int getDireccion(int dire);
 private:
     int x; //cordenada x
     int y; //cordenada y
     bool activo;
+    int direccion;
 };
 
 Cuadrito::Cuadrito() //constructor default
@@ -23,13 +26,15 @@ Cuadrito::Cuadrito() //constructor default
     x=0;
     y=0;
     activo=false;
+    direccion=2;
 }
 
-Cuadrito::Cuadrito(int iX, int iY, bool iActivo) //constructor con X y Y
+Cuadrito::Cuadrito(int iX, int iY, bool iActivo, int direcion) //constructor con X y Y
 {
     x=iX;
     y=iY;
     activo=iActivo;
+    direccion=2;
 }
 
 void Cuadrito::setX(int iX) //modificar X
@@ -47,6 +52,11 @@ void Cuadrito::setActivo(bool iActivo)
     activo=iActivo;
 }
 
+void Cuadrito::setDireccion(int dire)
+{
+    direccion=dire;
+}
+
 int Cuadrito::getX() //obtener X
 {
     return x;
@@ -60,5 +70,10 @@ int Cuadrito::getY() //obtener Y
 bool Cuadrito::getActivo()
 {
     return activo;
+}
+
+int Cuadrito::getDireccion()
+{
+    return direccion;
 }
 #endif // CUADRITO_H_INCLUDED
