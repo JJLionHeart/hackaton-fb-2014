ec49a60ae7e2dc79dfb5f2debe1f2ebc0b521c91
JJLionHeart <jj.lopezjaimez@gmail.com>
2014-10-24 20:26:41 -0500

    Tablero actualizado
    Elias es un noob

--- Tablero/cTablero.h
+++ Tablero/cTablero.h
@@ -1,33 +1,49 @@
 #ifndef CTABLERO_H_INCLUDED
 #define CTABLERO_H_INCLUDED
 #include <stdlib.h>
+#include <stdio.h>
 #include <conio.h>
 #include <windows.h>
 #include "cuadrito.h"
 class tablero{
 public:
+    tablero();
     void agregar(Cuadrito c);
     void refrescar();
-    void settablero(char c[][]);
+    void LimpiarTablero();
+    void Destruir();
 private:
-    unsigned char cTablero[25][80];
-    char c=254;
+    unsigned char **Tablero;
+
 
 };
-
-void tablero::settablero(char c[][]){
-    cTablero=c;
+tablero::tablero(){
+Tablero = (unsigned char**) malloc(sizeof(char)*25);
+for(int i=0;i<25;i++){
+    Tablero[i]=(unsigned char*)malloc(sizeof(char)*80);
+}
+}
+void tablero::LimpiarTablero(){
+    for(int i=0;i<25;i++){
+        for(int j=0;j<80;j++)
+            Tablero[i][j]=32;
+    }
 }
 
-void refrescar(){
+void tablero::refrescar(){
     system("cls");
-    printf("%s", cTablero)
+    printf("%s", Tablero);
 }
 
 void tablero::agregar(Cuadrito c){
-    int x=Cuadrito.getX();
-    int y=Cuadrito.getY();
-    cTablero[y][x]=c;
+    int x=c.getX();
+    int y=c.getY();
+    Tablero[y][x]=254;
+}
+void tablero::Destruir(){
+    for(int i=0;i<25;i++)
+        free(Tablero[i]);
+    free(Tablero);
 }
 
 
