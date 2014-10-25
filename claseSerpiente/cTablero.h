c91b0b0d6bc8294aa2a551cb7dc35ea0fbf85e3b
JJLionHeart <jj.lopezjaimez@gmail.com>
2014-10-24 19:20:16 -0500

    Serpiente
    cambio de las 7:20

---
+++ claseSerpiente/cTablero.h
@@ -0,0 +1,21 @@
+#ifndef CTABLERO_H_INCLUDED
+#define CTABLERO_H_INCLUDED
+
+class tablero{
+public:
+    void agregar(Cuadrito c);
+    void refrescar();
+private:
+    unsigned char cTablero[25][80];
+    char c=254;
+
+};
+
+void tablero::agregar(Cuadrito c){
+    int x=Cuadrito.getX();
+    int y=Cuadrito.getY();
+    cTablero[y][x]=c;
+}
+
+
+#endif // CTABLERO_H_INCLUDED
