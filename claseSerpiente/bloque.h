cd14788aec184492630c761e47c298b26872fd5c
JJLionHeart <jj.lopezjaimez@gmail.com>
2014-11-09 21:00:45 -0600

    Comentadas las clases bloque y tablero

--- claseSerpiente/bloque.h
+++ claseSerpiente/bloque.h
@@ -2,39 +2,43 @@
 #define BLOQUE_H_INCLUDED
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
-#include <windows.h>
-#include <iostream>
 
-#include "cTablero.h"
 
+
+/*
+Clase Bloque, Snake 2 v 2 Juan José López Jaimez y José Humberto Guevara Martinez
+Esta clase es para representar el objeto cuadrito que hace crecer a la snake cuando esta choca con el
+se incluyen funciones que generan numeros aleatorios para poder poner el cuadro en un lugar al azar del tablero*/
 class bloque{
 public:
+    /* Constructores default y con parámetros*/
     bloque();
     bloque(int iX, int iY);
+    /*
+        Métodos de acceso y modificación
+    */
     void setX(int iX);
     void setY(int iY);
     int getX();
     int getY();
+    //Función para asignar coordenadas al azar al bloque
     void cambiaLugar();
 private:
     int x; //cordenada x
     int y; //cordenada y
 };
-void bloque::cambiaLugar(){
-
-    x=rand()%78+1;
-    y=rand()%23+1;
-
-}
-
+//Definición de los constructores
 bloque::bloque()
 {
     srand (time(NULL));
     x=rand()%78+1;
     y=rand()%23+1;
 }
-
-
+bloque::bloque(int iX, int iY){
+    x=iX;
+    y=iY;
+}
+//definicion de los métodos de acceso y modificación
 int bloque::getX()
 {
     return x;
@@ -53,6 +57,13 @@ void bloque::setX(int iX)
 void bloque::setY(int iY)
 {
     y=iY;
+}
+//Definicion del metodo de cambio de coordenadas
+void bloque::cambiaLugar(){
+
+    x=rand()%78+1;
+    y=rand()%23+1;
+
 }
 
 #endif // BLOQUE_H_INCLUDED
--- claseSerpiente/cTablero.h
+++ claseSerpiente/cTablero.h
@@ -8,64 +8,76 @@
 #include "bloque.h"
 class tablero{
 public:
+    //constructor default
     tablero();
-    void agregar(Cuadrito c);
-    void agregar(bloque y);
-    void agregar(int x, int y);
-    void refrescar();
-    void LimpiarTablero();
-    bool checar(int a,int b);
-    bool checarcolision(int a,int b);
-    void bordes();
+    //funciones hechas para agregar cosas al tablero
+    void agregar(Cuadrito c);//para agregar al tablero objetos de la clase cuadrito
+    void agregar(bloque y);//para agregar al tablero el bloque que hace crecer la snake
+    void agregar(int x, int y);//funcion para agregar un caracter 254 en una coordenada
+    void bordes();//agrega en el tablero los margenes de pantalla
+    //Funciones que manipulan el tablero
+    void refrescar();//borra lo que está en pantalla y vuelve a dibujar el tablero
+    void LimpiarTablero();//borra todos los elementos del arreglo tablero
+    //Funciones de coolisiones
+    bool checar(int a,int b);//revisa si la coordenada (a,b) está ocupada por un caracter 254
+    bool checarcolision(int a,int b);//revisa si la coordenada (a,b) está ocupada por un caracter 254
+    //las 2 funciones anteriores hay que fusionarlas en una sola
+
 
 private:
-    unsigned char Tablero[26][80];
+    unsigned char Tablero[25][80];//el tablero es un arreglo de caracteres que se imprime en la ventana msdos
+    //las dimensiones de una ventana msdos son 25 filas por 80 columnas
 
 
 };
+//constructor default:
+tablero::tablero(){
+    //Inicializa el tablero en espacios (caracter 32) y coloca el caracter nulo al final
+for(int i=0;i<25;i++)
+    for(int j=0;j<80;j++)
+    Tablero[i][j]=32;
+    Tablero[24][79]='\0';
+}
+//funciones que agregan cosas al tablero
+void tablero::agregar(Cuadrito c){
+    //agrega un objeto de la clase cuadrito al tablero en funcion de las coordenadas de este objeto
+    int x=c.getX();
+    int y=c.getY();
+    Tablero[y][x]=c.getC();
+}
+void tablero::agregar(bloque y){
+    //agrega un objeto bloque al tablero, el objeto bloque es el que hace crecer a la serpiente
+    int x,a;
+    do{
+
+x=y.getX();
+a=y.getY();
+if(!checar(x,a))
+    y.cambiaLugar();
+    }while(!checar(x,a));//se tiene que revisar que la posicion en la que se intenta poner el bloque
+                        // si la posicion ya esta ocupada se manda llamar al metodo cambiar lugar hasta que quede en una posicion sin ocupar
+Tablero[a][x]=2;
+}
+void tablero::agregar(int x, int y){//agrega un caracter 254 en la coordenada x,y
+    Tablero[y][x]=254;
+}
 void tablero::bordes()
 {
-    for(int i=0;i<80;i++)
+    //dibuja los bordes (el marco) del tablero
+    for(int i=0;i<80;i++)//horizontales
     {
         Tablero[0][i]=254;
         Tablero[24][i]=254;
     }
-    for(int j=0;j<25;j++)
+    for(int j=0;j<25;j++)//verticales
     {
         Tablero[j][0]=254;
         Tablero[j][79]=254;
     }
 
 }
-
-bool tablero::checar(int a,int b){
-if(Tablero[b][a]!= 2)
-    return true;
-else
-    return false;
-}
-bool tablero::checarcolision(int a,int b){
-if(Tablero[b][a]== 254)
-    return true;
-else
-    return false;
-}
-
-void tablero::agregar(bloque y){
-    int x,a;
-    do{
-x=y.getX();
-a=y.getY();
-    }while(!checar(x,a));
-Tablero[a][x]=254;
-}
-tablero::tablero(){
-for(int i=0;i<25;i++)
-    for(int j=0;j<80;j++)
-    Tablero[i][j]=32;
-    Tablero[24][79]='\0';
-}
-void tablero::LimpiarTablero(){
+//funciones que manipulan el tablero
+void tablero::LimpiarTablero(){//borra todo el contenido del tablero, poniendo en cada posicion un espacio (caracter 32)
     for(int i=0;i<25;i++){
         for(int j=0;j<80;j++)
             Tablero[i][j]=32;
@@ -73,24 +85,39 @@ void tablero::LimpiarTablero(){
     Tablero[24][79]='\0';
 }
 
-void tablero::refrescar(){
-    system("cls");
+void tablero::refrescar(){//borra lo que esté en pantalla y vuelve a imprimir el tablero
+    system("cls");//borra pantalla
     bordes();
     Tablero[24][79]='\r';
     Tablero[25][0]='\0';
-    printf("%s", Tablero);
+    printf("%s", Tablero);//imprime tablero
 
 }
-
-void tablero::agregar(Cuadrito c){
-    int x=c.getX();
-    int y=c.getY();
-    Tablero[y][x]=c.getC();
+//funciones para colisiones
+bool tablero::checar(int a,int b){
+    //funcion para checar si una cierta posicion está ocupada o no
+    // si no está ocupada devuelve un true
+    // si está ocupada devuelve un false
+if(Tablero[b][a]!= 254)
+    return true;
+else
+    return false;
+}
+bool tablero::checarcolision(int a,int b){
+    //esta funcion hace exactamente lo mismo que checar() pero el true false está alreves, hay que eliminarla
+if(Tablero[b][a]== 254)
+    return true;
+else
+    return false;
 }
 
-void tablero::agregar(int x, int y){
-    Tablero[y][x]=254;
-}
+
+
+
+
+
+
+
 
 
 
